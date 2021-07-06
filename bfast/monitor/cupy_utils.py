7fa990fec16e041988dd097d326cb51750c4332b
12rambau <pierrick.rambaud49@gmail.com>
2021-07-06 13:43:41 +0000

    drop the use of cupy array for utils I retreive 100s on the peru small computation

--- bfast/monitor/cupy_utils.py
+++ bfast/monitor/cupy_utils.py
@@ -34,46 +34,6 @@ def get_critval(h, period, level, mr):
 
 _find_index_date = utils._find_index_date
 
-def crop_data_dates(data, dates, start, end):
-    """ Crops the input data and the associated
-    dates w.r.t. the provided start and end
-    datetime object.
-
-    Parameters
-    ----------
-    data: ndarray of shape (N, W, H)
-        Here, N is the number of time
-        series points per pixel and W
-        and H are the width and the height
-        of the image, respectively.
-    dates : list of datetime objects
-        Specifies the dates of the elements
-        in data indexed by the first axis
-        n_chunks : int or None, default None
-    start : datetime
-        The start datetime object
-    end : datetime
-        The end datetime object
-
-    Returns
-    -------
-    Returns: data, dates
-        The cropped data array and the
-        cropped list. Only those images
-        and dates that are with the start/end
-        period are contained in the returned
-        objects.
-    """
-
-    start_idx = _find_index_date(dates, start)
-    end_idx = _find_index_date(dates, end)
-
-    data_cropped = data[start_idx:end_idx, :, :]
-    dates_cropped = list(cp.array(dates)[start_idx:end_idx])
-
-    return data_cropped, dates_cropped
-
-
 def compute_lam(N, hfrac, level, period):
     
     check(hfrac, period, 1 - level, "max")
